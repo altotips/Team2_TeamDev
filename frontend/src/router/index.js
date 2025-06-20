5088402d13ef48a719676ad47f1197fb0309ec8a
nayu <nayu.sato@gigxit.co.jp>
2025-06-20 16:46:02 +0900

    タイムラインファイル作成

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -21,7 +21,7 @@ const router = createRouter({
     {
       path: '/register',
       name: 'register',
-      component: () => import('../views/RegisterView.vue'),
+      component: () => import('../views/RegisterView.vue')
 
     },
 
