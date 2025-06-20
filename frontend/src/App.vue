ecf5fd904de5199925664caa5a8918cd7f1b842b
uchikura <uchikura@example.com>
2025-06-20 16:55:18 +0900

    PostController、ストアはまだ終わってない

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import Header from './views/Header.vue'
 import Footer from './views/Footer.vue'
 </script>
