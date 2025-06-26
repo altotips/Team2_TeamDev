c321c18c95601bb30beaa1215534393fcf7f8e93
shimizu shouta <shota.shimizu@gigxit.co.jp>
2025-06-26 10:50:04 +0900

    検索画面編集

--- backend/src/main/java/com/example/demo/sns/config/WebConfig.java
+++ backend/src/main/java/com/example/demo/sns/config/WebConfig.java
@@ -2,9 +2,10 @@ package com.example.demo.sns.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@CrossOrigin
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	@Value("${upload.path}")
