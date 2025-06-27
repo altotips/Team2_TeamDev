22b343cd99c203909a8acac9637df5f6b8691847
okada <okada@ia.com>
2025-06-27 13:03:34 +0900

    ＃の候補出すやつ（ひらがなとカタカナのみ）

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 // ここでbaseURLと通信するオブジェクトがjsonであることを設定している。
 // 使うときはaxios.get('/posts')で http://localhost:8080/api/posts になる。
 const instance = axios.create({
-  baseURL: 'http://localhost:8080/api', // Spring Bootのエンドポイントに合わせて
+  // baseURL: 'http://localhost:8080/api', // Spring Bootのエンドポイントに合わせて
+  baseURL: 'http://192.168.137.1:8080/api', // Spring Bootのエンドポイントに合わせて
   headers: {
     'Content-Type': 'application/json',
   },
